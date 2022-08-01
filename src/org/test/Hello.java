36e1481e25118002c29c5cc7fe87dc09ff26d226
missri-saqib <saqib4461103@gmail.com>
2022-08-02 01:21:39 +0530

    Data Added

---
+++ src/org/test/Hello.java
@@ -0,0 +1,24 @@
+package org.test;
+
+public class Hello {
+	public static void main(String[] args) {
+		int a[]= {10,20,30,40,55};
+		int b[]= {10,25,40,35,55,20};
+		for (int i = 0; i < a.length; i++) {    
+			for (int j = 0; j < b.length; j++) { 
+				if (a[i]==b[j]) {                
+					System.out.println(a[i]);    
+				
+				} else {
+
+				continue;
+				
+				}
+		}
+		
+		}
+		
+		}
+
+
+}
